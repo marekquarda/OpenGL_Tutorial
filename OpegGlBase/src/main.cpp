acb9283a4802188a9de379badd3aaecd11c23ba5
marekquarda <mquarda@centrum.cz>
2025-01-08 17:28:14 +0100

    jjj

--- OpegGlBase/src/main.cpp
+++ OpegGlBase/src/main.cpp
@@ -3,5 +3,23 @@
 #include <glfw/glfw3.h>
 
 int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800,800,"OpenGl Base",NULL, NULL);
+	if(window == NULL) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
