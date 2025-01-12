719314d827882217b6cff4991b15effa08a966f6
marekquarda <mquarda@centrum.cz>
2025-01-12 17:27:27 +0100

    Image load

--- OpenGlImg/src/main.cpp
+++ OpenGlImg/src/main.cpp
@@ -5,23 +5,21 @@
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
-//#include "stb_image.h"
+#include "stb_image.h"
 
-	GLfloat vertices[] = 
-	{ //               COORDINATES                  /     COLORS           //
-		-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-		0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-		-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-		0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-		0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
-	};	
-	GLuint indices[] = 
-	{
-		0, 3, 5, // Lower left triangle
-		3, 2, 4, // Lower right triangle
-		5, 4, 1 // Upper triangle
-	};
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
+};
+
+GLuint indices[] = 
+{
+	0, 2, 1, // Upper triangle
+	0, 3, 2 // Lower triangle
+};
 
 
 int main() {
@@ -67,9 +65,9 @@ int main() {
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6* sizeof(float), (void*)(3*sizeof(float)));
-
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3*sizeof(float))); 
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6*sizeof(float)));
 
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
@@ -78,7 +76,34 @@ int main() {
 
 	// Gets ID of uniform called "scale"
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	// Texture 
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("../pop_cat.png", &widthImg, &heightImg, &numColCh, 0);
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// float flatColor[]= {1.0f, 1.0f, 1.0f, 1.0f};
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+
+	glTexImage2D(GL_TEXTURE_2D,0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
 	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
 
 	while(!glfwWindowShouldClose(window)) {
 		// Specify the color of the background
@@ -86,9 +111,10 @@ int main() {
 		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
-		//shaderProgram.Activate();
+		shaderProgram.Activate();
 		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
 		glUniform1f(uniID, 0.5f);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
@@ -103,6 +129,7 @@ int main() {
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
--- OpenGlImg/src/shaders.h
+++ OpenGlImg/src/shaders.h
@@ -9,12 +9,15 @@ R".(
 
     layout(location = 0) in vec3 aPos;
     layout(location = 1) in vec3 aColor;
+    layout(location = 2) in vec2 aTex;  
 
     out vec3 color;
+    out vec2 texCoord;
 
     void main() {
 	    gl_Position = vec4(aPos.x, aPos.y, aPos.y, 1.0);
 	    color = aColor;
+        texCoord = aTex;
     }
 ).";
 
@@ -24,9 +27,12 @@ R".(
 
     out vec4 FragColor;
     in vec3 color;
+    in vec2 texCoord;
+    uniform sampler2D tex0;
 
     void main() {
-	    FragColor = vec4(color,1.0f);
+	    //FragColor = vec4(color,1.0f);
+        FragColor = texture(tex0, texCoord);
     }
 ).";
 
