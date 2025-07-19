cae4f1ceec48e62fbec4fb0d11f17ab36b7cc423
marekquarda <mquarda@centrum.cz>
2025-07-19 18:31:25 +0200

    raySphere method added

--- raytracer/src/main.cpp
+++ raytracer/src/main.cpp
@@ -2,6 +2,7 @@
 #include <stdint.h> 
 #include <vector>
 #include "stbi_image_write.h"
+#include <glm/glm.hpp>
 
 
 struct Frame{
@@ -34,8 +35,46 @@ void saveFrame(std::string const&file, Frame const&f) {
     std::cerr << "storing screenshot to: \"" << file << "\"" << std::endl;
 }
 
+struct Sphere{
+    glm::vec3 C;
+    float     R;
+};
+
+struct Ray
+{
+    glm::vec3 S;
+    glm::vec3 D;
+};
+
+
+float raySphereIntersection(Sphere const&s, Ray const&ray) {
+    float a = glm::dot(ray.D,ray.D);
+    float b = 2*glm::dot(ray.D, ray.S-s.C);
+    float c = glm::dot(ray.S-s.C,ray.S-s.C)-s.R*s.R;
+    float d = b*b-4*a*c;
+    if(d<0.f) return -1.f;
+    float t1 = (-b-glm::sqrt(d))/(2*a);
+    float t2 = (+b-glm::sqrt(d))/(2*a);
+    if (t1<0.f) return t2;
+    if (t2<0.f) return t1;
+    return glm::min(t1,t2);
+}
+
 int main(int args, char*argv[]) {
-    Frame frame = Frame(256,256,3);
+    Frame frame = Frame(1024,1024,3);
+    for (uint32_t y=0;y<frame.width;++y) {
+        for(u_int32_t x=0;x<frame.height;++x) {
+            float fx = (float)x/(float)(frame.width-1)*2.f-1.f;
+            float fy = (float)y/(float)(frame.height-1)*2.f-1.f;
+            auto S = glm::vec3(0.f);
+            auto D = glm::normalize(glm::vec3(fx, fy, -1.f));
+
+            frame.data[(y*frame.width+x)*frame.channels+0] = (uint8_t) glm::clamp(255.f*(D.x*.5f+.5f),0.f,255.f);
+            frame.data[(y*frame.width+x)*frame.channels+1] = (uint8_t) glm::clamp(255.f*(D.y*.5f+.5f),0.f,255.f);
+            
+        }
+    }
+
     saveFrame("output.png", frame);
     return 1;
 }
